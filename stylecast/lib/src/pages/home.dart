c60f67f0513bccff9e1a6196f3ce087821090057
Nathan Kim <nkimfromhome@gmail.com>
2024-05-01 00:29:13 -0700

    Connection to OpenWeather API
    1. Connects to OpenWeather API
    2. Displays this on the home screen
    3. Had to comment out the entire app.dart file to make this happen

--- stylecast/lib/src/pages/home.dart
+++ stylecast/lib/src/pages/home.dart
@@ -2,31 +2,37 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:stylecast/src/pages/login.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase login"),
-      ),
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-          if (!snapshot.hasData) {
-            return LoginWidget();
-          } else {
-            return Center(
-              child: Column(
-                children: [
-                  Text("${snapshot.data.displayName}, Welcome"),
-                ],
-              ),
-            );
-          }
-        },
-      ),
-    );
-  }
-}
+// class Home extends StatelessWidget {
+//   const Home({Key? key}) : super(key: key);
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     throw UnimplementedError();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Firebase login"),
+//       ),
+//       body: StreamBuilder(
+//         stream: FirebaseAuth.instance.authStateChanges(),
+//         builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+//           if (!snapshot.hasData) {
+//             return LoginWidget();
+//           } else {
+//             return Center(
+//               child: Column(
+//                 children: [
+//                   Text("${snapshot.data.displayName}, Welcome"),
+//                 ],
+//               ),
+//             );
+//           }
+//         },
+//       ),
+//     );
+//  }
