aaef0cc567a67053e2be9a11d9d4e8c50ebf6782
Nathan Kim <nkimfromhome@gmail.com>
2024-05-01 23:31:25 -0700

    Newest Changes
    home screen fetching data from OpenWeather

--- stylecast/lib/src/pages/home.dart
+++ stylecast/lib/src/pages/home.dart
@@ -1,31 +1,107 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:stylecast/src/pages/login.dart';
+import 'package:stylecast/src/pages/weather_service.dart';
 
-// class Home extends StatelessWidget {
-//   const Home({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Firebase login"),
-//       ),
-//       body: StreamBuilder(
-//         stream: FirebaseAuth.instance.authStateChanges(),
-//         builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-//           if (!snapshot.hasData) {
-//             return LoginWidget();
-//           } else {
-//             return Center(
-//               child: Column(
-//                 children: [
-//                   Text("${snapshot.data.displayName}, Welcome"),
-//                 ],
-//               ),
-//             );
-//           }
-//         },
-//       ),
-//     );
-//  }
+class _HomePageState extends State<HomePage> {
+  final WeatherService _weatherService = WeatherService();
+  String temperature = '';
+  String weatherDescription = 'Loading...';
+  String weatherIcon = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadWeather();
+  }
+
+  Future<void> _loadWeather() async {
+    try {
+      var weatherData = await _weatherService.getWeather("San Jose");
+      setState(() {
+        temperature = '${weatherData['main']['temp']}°C';
+        weatherDescription = weatherData['weather'][0]['description'].toString();
+        weatherIcon = _mapWeatherConditionToIcon(weatherData['weather'][0]['main'].toString());
+      });
+    } catch (e) {
+      setState(() {
+        weatherDescription = "Failed to load weather data";
+      });
+    }
+  }
+
+  String _mapWeatherConditionToIcon(String condition) {
+    switch (condition) {
+      case 'Clear':
+        return '☀'; // Sunny
+      case 'Clouds':
+        return '☁'; // Cloudy
+      case 'Rain':
+        return '🌧'; // Rainy
+      case 'Snow':
+        return '❄'; // Snowy
+      default:
+        return '🌈'; // Default to something cheerful
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.sync, color: Colors.black),
+            onPressed: _loadWeather,
+          )
+        ],
+        title: Text('San Jose, CA', style: TextStyle(color: Colors.black)),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(weatherIcon, style: TextStyle(fontSize: 64)),
+                  SizedBox(height: 10),
+                  Text(temperature, style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+                  Text(weatherDescription, style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            ),
+            // _buildStylecastCard(),
+            // _buildHourlyForecast(),
+            // _buildWeeklyForecast(),
+          ],
+        ),
+      ),
+    );
+  }
+
+//Come back to these three methods
+  // Widget _buildStylecastCard() {
+  //   // Implement this method based on your specific UI requirements
+  // }
+
+  // Widget _buildHourlyForecast() {
+  //   // Implement this method based on your specific UI requirements
+  // }
+
+  // Widget _buildWeeklyForecast() {
+  //   // Implement this method based on your specific UI requirements
+  // }
+}
