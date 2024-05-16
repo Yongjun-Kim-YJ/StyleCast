5c5b7b2e6d1b198d1bfcd3daacef9ab2aa9af01d
Yongjun-Kim-YJ <yongjun.j.kim@gmail.com>
2024-05-16 04:23:02 -0700

    facebook login removed and google login button changed

--- stylecast/lib/src/pages/login.dart
+++ stylecast/lib/src/pages/login.dart
@@ -146,28 +146,19 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                padding: const EdgeInsets.symmetric(horizontal: 0),
                 child: Column(
                   children: [
+                    SizedBox(height: 20),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         GestureDetector(
                           onTap: _signInWithGoogle,
                           child: Image.asset(
-                            'assets/images/google_logo.png',
-                            width: 62,
-                            height: 62,
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            // 애플 로그인 로직 추가
-                          },
-                          child: Image.asset(
-                            'assets/images/facebook_logo.png',
-                            width: 62,
-                            height: 62,
+                            'assets/images/google_login.png',
+                            width: 280,
+                            height: 49,
                           ),
                         ),
                       ],
@@ -180,7 +171,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(27),
                         ),
-                        minimumSize: Size(271, 46),
+                        fixedSize: Size(271, 46),
                       ),
                       child:
                           Text('Login', style: TextStyle(color: Colors.white)),
